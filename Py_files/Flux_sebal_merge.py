239724eafbfa75db9e115c840d56d68eb73c07a9
rouhinmitra <rouhinmitra@gmail.com>
2024-07-23 10:21:24 -0700

    Adding new file

---
+++ Py_files/Flux_sebal_merge.py
@@ -0,0 +1,33 @@
+#%%
+import pandas as pd 
+import os 
+import numpy as np 
+
+dir_flux="D:\\Backup\\Rouhin_Lenovo\\US_project\\GEE_SEBAL_Project\\Csv_Files\\AF_dT_unzip\\"
+dir_sebal="D:\\Backup\\Rouhin_Lenovo\\US_project\\Untitled_Folder\\Data\\Inst_project\\dT_sebal\\"
+
+def read_flux():
+    """
+    Read amerflux data from folder: 
+    """
+
+    return 
+
+def read_sebal():
+
+
+    return 
+
+def merge_inst_sebal():
+    """
+    Merge during landsat overpass 
+    """
+
+    return 
+
+def 
+#%%
+
+# temp
+#%%
+
